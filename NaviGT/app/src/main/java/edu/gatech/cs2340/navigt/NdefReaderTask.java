75ff8550ce041008cc281cbdb8833501b86e7e8d
Mohit Doshi <mohit.dosi@gmail.com>
2018-10-20 11:02:59 -0400

    Update gradle

---
+++ NaviGT/app/src/main/java/edu/gatech/cs2340/navigt/NdefReaderTask.java
@@ -0,0 +1,12 @@
+package edu.gatech.cs2340.navigt;
+
+import java.util.Arrays;
+import java.io.*;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.AsyncTask;
+import android.util.Log;
+
+
